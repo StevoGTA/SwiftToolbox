ec5cb0a57c216da06714d632abeb55f1dec6ab8d
Stevo <stevo@sunsetmagicwerks.com>
2020-02-07 11:27:25 -0800

    Added more Locking Primitives

--- Source/Concurrency/LockingPrimitives.swift
+++ Source/Concurrency/LockingPrimitives.swift
@@ -1,5 +1,5 @@
 //
-//  LockingPrimitive.swift
+//  LockingPrimitives.swift
 //  Swift Toolbox
 //
 //  Created by Stevo on 11/6/19.
@@ -7,8 +7,34 @@
 //
 
 //----------------------------------------------------------------------------------------------------------------------
-// MARK: LockingPrimitive
-public class LockingPrimitive<T : Numeric> {
+// MARK: LockingValue
+public class LockingValue<T> {
+
+	// MARK: Properties
+	public	var	value :T { return self.lock.read() { return self.valueInternal } }
+
+	private	let	lock = ReadPreferringReadWriteLock()
+
+	private	var	valueInternal :T
+
+	// MARK: Lifecycle methods
+	//------------------------------------------------------------------------------------------------------------------
+	public init(_ initialValue :T) {
+		// Store
+		self.valueInternal = initialValue
+	}
+
+	// MARK: Instance methods
+	//------------------------------------------------------------------------------------------------------------------
+	public func set(_ value :T) {
+		// Set
+		self.lock.write() { self.valueInternal = value }
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - LockingNumeric
+public class LockingNumeric<T : Numeric> {
 
 	// MARK: Properties
 	public	var	value :T { return self.lock.read() { return self.valueInternal } }
