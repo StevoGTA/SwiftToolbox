66c119d310f6cdfdf0bdb380e7c5e00e1b9d8c3e
Stevo <stevo@sunsetmagicwerks.com>
2020-04-08 14:46:34 -0700

    Reworked HTTPEndpointRequest to be more Swifty.

--- Source/Networking/HTTPEndpointRequest.swift
+++ Source/Networking/HTTPEndpointRequest.swift
@@ -9,19 +9,38 @@
 import Foundation
 
 //----------------------------------------------------------------------------------------------------------------------
+// MARK: HTTPEndpointRequestError
+enum HTTPEndpointRequestError : Error {
+	case unableToProcessResponseData
+}
+
+extension HTTPEndpointRequestError : LocalizedError {
+
+	// MARK: Properties
+	public	var	errorDescription :String? {
+						// What are we
+						switch self {
+							case .unableToProcessResponseData:	return "Unable to process response data"
+						}
+					}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
 // MARK: HTTPEndpointRequest
-struct HTTPEndpointRequest {
+class HTTPEndpointRequest {
 
-	// MARK: Options
-	public	struct Options : OptionSet {
+	// MARK: Types
+	enum MultiValueQueryParameterHandling {
+		case useComma
+		case repeatKey
+	}
 
-//				static	public	let	multiValueQueryParametersUseMultipleKeys = Options(rawValue: 1 << 0)
+	struct Options {
 
-						public	let	rawValue :Int
-
-				// MARK: Lifecycle methods
-				public init(rawValue :Int) { self.rawValue = rawValue }
-			}
+		// MARK: Properties
+		let	multiValueQueryParameterHandling = MultiValueQueryParameterHandling.repeatKey
+		let	maximumURLLength = 1024
+	}
 
 	// MARK: Properties
 	let	method :HTTPEndpointMethod
@@ -74,7 +93,7 @@ struct HTTPEndpointRequest {
 
 	// MARK: Instance methods
 	//------------------------------------------------------------------------------------------------------------------
-	func urlRequest(with serverPrefix :String, options :Options = []) -> URLRequest {
+	func urlRequest(with serverPrefix :String, options :Options = Options()) -> URLRequest {
 		// Setup
 		var	urlRequest = URLRequest(url: URL(string: "\(serverPrefix)\(self.path)")!)
 		switch self.method {
@@ -94,4 +113,110 @@ struct HTTPEndpointRequest {
 
 		return urlRequest
 	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - HTTPEndpointRequestResultsHandler
+protocol HTTPEndpointRequestResultsHandler {
+
+	// MARK: Types
+	associatedtype Results
+
+	// MARK: Methods
+	func transformResults(data :Data?, response :HTTPURLResponse?, error :Error?) -> Results
+	func resultsProc(_ results :Results) -> Void
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - BasicHTTPEndpointRequest
+class BasicHTTPEndpointRequest : HTTPEndpointRequest, HTTPEndpointRequestResultsHandler {
+
+	// MARK: HTTPEndpointRequestResultsHandler implementation
+	typealias Results = Error?
+
+	func transformResults(data: Data?, response: HTTPURLResponse?, error: Error?) -> Error? { error }
+
+	func resultsProc(_ results: Error?) { self.completionProc(results) }
+
+	// MARK: Properties
+	var	completionProc :(_ error :Error?) -> Void = { _ in }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - HeadHTTPEndpointRequest
+class HeadHTTPEndpointRequest : HTTPEndpointRequest, HTTPEndpointRequestResultsHandler {
+
+	// MARK: HTTPEndpointRequestResultsHandler implementation
+	typealias Results = (headers :[AnyHashable : Any]?, error :Error?)
+
+	func transformResults(data: Data?, response: HTTPURLResponse?, error: Error?) ->
+			(headers: [AnyHashable : Any]?, error: Error?) { ( response?.allHeaderFields, error ) }
+
+	func resultsProc(_ results: (headers: [AnyHashable : Any]?, error: Error?)) {
+		// Call proc
+		self.headersProc(results.headers, results.error)
+	}
+
+	// MARK: Properties
+	var	headersProc :(_ headers :[AnyHashable : Any]?, _ error :Error?) -> Void = { _,_ in }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - StringHTTPEndpointRequest
+class StringHTTPEndpointRequest : HTTPEndpointRequest, HTTPEndpointRequestResultsHandler {
+
+	// MARK: HTTPEndpointRequestResultsHandler implementation
+	typealias Results = (string :String?, error :Error?)
+
+	func transformResults(data: Data?, response: HTTPURLResponse?, error: Error?) -> (string: String?, error: Error?) {
+		// Handle results
+		if data != nil {
+			// Try to copmose string from data
+			if let string = String(data: data!, encoding: .utf8) {
+				// Success
+				return (string, nil)
+			} else {
+				// Unable to transform results
+				return (nil, HTTPEndpointRequestError.unableToProcessResponseData)
+			}
+		} else {
+			// Error
+			return (nil, error)
+		}
+	}
+
+	func resultsProc(_ results: (string: String?, error: Error?)) { self.stringProc(results.string, results.error) }
+
+	// MARK: Properties
+	var	stringProc :(_ string :String?, _ error :Error?) -> Void = { _,_ in }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - JSONHTTPEndpointRequest
+class JSONHTTPEndpointRequest<T> : HTTPEndpointRequest, HTTPEndpointRequestResultsHandler {
+
+	// MARK: HTTPEndpointRequestResultsHandler implementation
+	typealias Results = (info :T?, error :Error?)
+
+	func transformResults(data: Data?, response: HTTPURLResponse?, error: Error?) -> Results {
+		// Handle results
+		if data != nil {
+			// Try to compose info from data
+			if let info = try? JSONSerialization.jsonObject(with: data!, options: []) as? T {
+				// Success
+				return (info, nil)
+			} else {
+				// Unable to transform results
+				return (nil, HTTPEndpointRequestError.unableToProcessResponseData)
+			}
+		} else {
+			// Error
+			return (nil, error)
+		}
+	}
+
+	func resultsProc(_ results: (info: T?, error: Error?)) { self.infoProc(results.info, results.error) }
+
+	// MARK: Properties
+	var	infoProc :(_ info :T?, _ error :Error?) -> Void = { _,_ in }
 }
