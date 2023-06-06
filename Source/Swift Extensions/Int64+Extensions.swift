f1b39d3d9fcb62260434d26e5a4c45aaff4e65b3
Stevo <stevo@sunsetmagicwerks.com>
2023-06-06 16:20:02 -0700

    Added Int64 extensions and XMLNode extensions.

---
+++ Source/Swift Extensions/Int64+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Int64+Extensions.swift
+//  Swift Toolbox
+//
+//  Created by Stevo on 6/6/23.
+//  Copyright © 2023 Stevo Brock. All rights reserved.
+//
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: Int64 extension
+extension Int64 {
+
+	// MARK: Properties
+	static	let	`nil` :Int64? = nil
+
+	// MARK: Lifecycle methods
+	//------------------------------------------------------------------------------------------------------------------
+	public init?(_ string :String?) {
+		// Check if string is nil
+		guard string != nil else { return nil }
+
+		self.init(string!)
+	}
+}
---
+++ Source/Swift Extensions/XMLNode+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  XMLNode+Extensions.swift
+//  Swift Toolbox
+//
+//  Created by Stevo on 6/6/23.
+//  Copyright © 2023 Stevo Brock. All rights reserved.
+//
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: XMLNode extension
+extension XMLNode {
+
+	// MARK: Properties
+	public	var	int64Value :Int64? { Int64(self.stringValue) }
+
+	// MARK: Instance methods
+	//------------------------------------------------------------------------------------------------------------------
+	public func firstChildElement(named name :String) -> XMLElement? {
+		// Return first child with matching name
+		return self.children?.first(where: { $0.name == name }) as? XMLElement
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	public func children(named name :String) -> [XMLNode] { self.children?.filter({ $0.name == name }) ?? [] }
+}
