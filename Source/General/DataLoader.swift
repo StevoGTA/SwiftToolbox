bb1841c873f98a9c058263bd35598edaabc355f9
Stevo <stevo@sunsetmagicwerks.com>
2024-03-15 18:57:15 -0700

    Added load(...) and loadInBackground(...) to DataLoader.

--- Source/General/DataLoader.swift
+++ Source/General/DataLoader.swift
@@ -36,5 +36,40 @@ import Foundation
 	@objc func load() throws -> Data { return try self.loadProc() }
 
 	//------------------------------------------------------------------------------------------------------------------
+	@objc func load(on queue :DispatchQueue = .global(qos: .userInitiated),
+			completionProc :@escaping(_ data :Data?, _ error :Error?) -> Void) {
+		// Queue
+		queue.async() { [weak self] in
+			// Catch errors
+			let	data :Data?
+			let	loadError :Error?
+			do {
+				// Load
+				data = try self?.loadProc()
+				loadError = nil
+			} catch {
+				// Error
+				data = nil
+				loadError = error
+			}
+
+			// Switch to main queue
+			DispatchQueue.main.async() { [weak self] in
+				// Check things
+				guard self != nil else { return }
+
+				// Call completion
+				completionProc(data, loadError)
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	@objc func loadInBackground(completionProc :@escaping(_ data :Data?, _ error :Error?) -> Void) {
+		// Load on default queue
+		load(completionProc: completionProc)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
 	@objc func cancel() { self.cancelProc() }
 }
