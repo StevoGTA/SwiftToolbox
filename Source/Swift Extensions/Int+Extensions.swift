411dfff42213b8fc532ebd737fef00d4b72aecd2
Stevo <stevo@sunsetmagicwerks.com>
2025-02-20 14:45:17 -0800

    Added more convenience initializers in Int+Extensions. Added Set::map(...) and minor reorganization in file.

--- Source/Swift Extensions/Int+Extensions.swift
+++ Source/Swift Extensions/Int+Extensions.swift
@@ -15,10 +15,42 @@ extension Int {
 
 	// MARK: Lifecycle methods
 	//------------------------------------------------------------------------------------------------------------------
+	public init?(_ int32 :Int32?) {
+		// Check if string is nil
+		guard int32 != nil else { return nil }
+
+		self.init(int32!)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	public init?(_ int64 :Int64?) {
+		// Check if string is nil
+		guard int64 != nil else { return nil }
+
+		self.init(int64!)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
 	public init?(_ string :String?) {
 		// Check if string is nil
 		guard string != nil else { return nil }
 
 		self.init(string!)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	public init?(_ uint32 :UInt32?) {
+		// Check if string is nil
+		guard uint32 != nil else { return nil }
+
+		self.init(uint32!)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	public init?(_ uint64 :UInt64?) {
+		// Check if string is nil
+		guard uint64 != nil else { return nil }
+
+		self.init(uint64!)
 	}
 }
