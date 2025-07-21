4c5edfc9693d94399487e2be85b3afdac79494bf
Stevo <stevo@sunsetmagicwerks.com>
2025-07-21 12:58:43 -0700

    Reworked ProgressTracker to cover for situations.

--- Source/General/ProgressTracker.swift
+++ Source/General/ProgressTracker.swift
@@ -42,20 +42,26 @@ public class SizeProgressTracker {
 
 		// MARK: Instance methods
 		//--------------------------------------------------------------------------------------------------------------
-		func composeTimeDeltaInfo(for date :Date) -> TimeDeltaInfo {
-			// Setup
-			let	currentSize = self.sizeProgress.currentSize
+		func composeTimeDeltaInfo(for date :Date) -> TimeDeltaInfo? {
+			// Check if should report info.  We only report info if progress is tracked and has changed
+			if (self.sizeProgress.currentSize > self.lastCurrentSize) || (self.sizeProgress.currentSize > 0) {
+				// Setup
+				let	currentSize = self.sizeProgress.currentSize
 
-			// Compose TimeDeltaInfo
-			let	timeDeltaInfo =
-						TimeDeltaInfo(deltaSize: currentSize - self.lastCurrentSize,
-								deltaTimeInterval: date.timeIntervalSince(self.lastDate))
+				// Compose TimeDeltaInfo
+				let	timeDeltaInfo =
+							TimeDeltaInfo(deltaSize: currentSize - self.lastCurrentSize,
+									deltaTimeInterval: date.timeIntervalSince(self.lastDate))
 
-			// Update
-			self.lastCurrentSize = currentSize
-			self.lastDate = date
+				// Update
+				self.lastCurrentSize = currentSize
+				self.lastDate = date
 
-			return timeDeltaInfo
+				return timeDeltaInfo
+			} else {
+				// Either progress is not being tracked, or hasn't changed in this cycle
+				return nil
+			}
 		}
 	}
 
@@ -63,21 +69,24 @@ public class SizeProgressTracker {
 	private class TimeSliceInfo {
 
 		// MARK: Properties
-		private(set)	var	transferRate = 0.0
+		private(set)	var	rate = 0.0
 		private(set)	var	count = 0
-						var	transferRatePerItem :Double { (count > 0) ? transferRate / Double(count) : 0.0 }
+						var	ratePerItem :Double { (count > 0) ? rate / Double(count) : 0.0 }
 
 		// MARK: Instance methods
 		//--------------------------------------------------------------------------------------------------------------
 		func add(_ timeDeltaInfo :SizeProgressInfo.TimeDeltaInfo) {
 			// Update
-			self.transferRate += Double(timeDeltaInfo.deltaSize) / timeDeltaInfo.deltaTimeInterval
+			self.rate += Double(timeDeltaInfo.deltaSize) / timeDeltaInfo.deltaTimeInterval
 			self.count += 1
 		}
 	}
 
 	// MARK: Properties
-	private	var	sizeProgressInfos = LockingArray<SizeProgressInfo>()
+	private	let	arraysLock = Lock()
+	private	var	sizeProgressInfos = [SizeProgressInfo]()
+	private	var	recentlyCompletedSizeProgressInfos = [SizeProgressInfo]()
+
 	private	var	timeSliceInfos = [TimeSliceInfo]()
 	private	var	timer :Timer!
 
@@ -97,35 +106,44 @@ public class SizeProgressTracker {
 
 	// MARK: Instance methods
 	//------------------------------------------------------------------------------------------------------------------
-	public func add(_ sizeProgress :SizeProgress) { self.sizeProgressInfos.append(SizeProgressInfo(sizeProgress)) }
+	public func add(_ sizeProgress :SizeProgress) {
+		// Add
+		self.arraysLock.perform({ self.sizeProgressInfos.append(SizeProgressInfo(sizeProgress)) })
+	}
 
 	//------------------------------------------------------------------------------------------------------------------
 	public func remove(_ sizeProgress :SizeProgress) {
-		// Remove
-		self.sizeProgressInfos.removeAll(where: {$0.sizeProgress === sizeProgress} )
+		// Move to recently completed
+		self.arraysLock.perform() {
+			// Try to find
+			if let index = self.sizeProgressInfos.firstIndex(where: { $0.sizeProgress === sizeProgress }) {
+				// Move
+				self.recentlyCompletedSizeProgressInfos.append(self.sizeProgressInfos.remove(at: index))
+			}
+		}
 	}
 
 	//------------------------------------------------------------------------------------------------------------------
 	public func info(for remainingSize :Int64, remainingItems :Int, parallelItems :Int) ->
-			(averageTransferRate :Double, estimatedTimeIntervalRemaining :TimeInterval?) {
+			(averageRate :Double, estimatedTimeIntervalRemaining :TimeInterval?) {
 		// Compose info
 		let	count = Double(self.timeSliceInfos.count)
 		guard count > 0.0 else { return (0.0, nil) }
 
-		let	averageTransferRate = self.timeSliceInfos.reduce(0.0, { $0 + $1.transferRate }) / count
+		let	averageRate = self.timeSliceInfos.reduce(0.0, { $0 + $1.rate }) / count
 
-		let	averageTransferRatePerItem = self.timeSliceInfos.reduce(0.0, { $0 + $1.transferRatePerItem }) / count
+		let	averageRatePerItem = self.timeSliceInfos.reduce(0.0, { $0 + $1.ratePerItem }) / count
 		let	estimatedTimeIntervalRemaining :TimeInterval?
-		if (remainingSize > 0) && (averageTransferRatePerItem > 0.0) && (parallelItems > 0) {
+		if (remainingSize > 0) && (averageRatePerItem > 0.0) && (parallelItems > 0) {
 			// Have info to calculate
 			estimatedTimeIntervalRemaining =
-					Double(remainingSize) / averageTransferRatePerItem / Double(min(remainingItems, parallelItems))
+					Double(remainingSize) / averageRatePerItem / Double(min(remainingItems, parallelItems))
 		} else {
 			// Nothing to do or no way to do it
 			estimatedTimeIntervalRemaining = nil
 		}
 
-		return (averageTransferRate, estimatedTimeIntervalRemaining)
+		return (averageRate, estimatedTimeIntervalRemaining)
 	}
 
 	// MARK: Private methods
@@ -136,7 +154,28 @@ public class SizeProgressTracker {
 
 		// Compose new time slice info
 		let	timeSliceInfo = TimeSliceInfo()
-		self.sizeProgressInfos.values.forEach() { timeSliceInfo.add($0.composeTimeDeltaInfo(for: date)) }
+		self.arraysLock.perform() {
+			// Process current SizeProgressInfos
+			self.sizeProgressInfos
+					.compactMap({ $0.composeTimeDeltaInfo(for: date) })
+					.forEach({ timeSliceInfo.add($0) })
+
+			// Process recently completed SizeProgressInfos
+			self.recentlyCompletedSizeProgressInfos
+					.compactMap({ sizeProgressInfo -> SizeProgressInfo.TimeDeltaInfo? in
+						//
+						guard sizeProgressInfo.lastCurrentSize < sizeProgressInfo.sizeProgress.totalSize else {
+							// Nothing to report
+							return nil
+						}
+
+						return SizeProgressInfo.TimeDeltaInfo(
+								deltaSize: sizeProgressInfo.sizeProgress.totalSize - sizeProgressInfo.lastCurrentSize,
+								deltaTimeInterval: date.timeIntervalSince(sizeProgressInfo.lastDate))
+					})
+					.forEach({ timeSliceInfo.add($0) })
+			self.recentlyCompletedSizeProgressInfos.removeAll()
+		}
 
 		// Update storage
 		self.timeSliceInfos.append(timeSliceInfo)
