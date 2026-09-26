e477e3bd81077303732b9600bbd99b83574f87ac
Stevo <stevo@monkey-tools.com>
2026-09-25 17:33:11 -0700

    Significant upgrades to TableViewBacking.

--- Source/UI/TableViewBacking.swift
+++ Source/UI/TableViewBacking.swift
@@ -8,6 +8,7 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 // MARK: TableViewBacking
+@MainActor
 public class TableViewBacking : NSObject {
 
 	// MARK: Item
@@ -15,8 +16,8 @@ public class TableViewBacking : NSObject {
 	class Item : NSObject {
 
 		// MARK: Properties
-		let	id :String
-		let	object :Any
+				let	id :String
+		@objc	let	object :Any
 
 		// MARK: Lifecycle methods
 		//--------------------------------------------------------------------------------------------------------------
@@ -29,22 +30,102 @@ public class TableViewBacking : NSObject {
 	}
 
 	// MARK: Properties
-	private	var	itemByItemID = [String : Item]()
+	@objc			var	count :Int { self.itemIDsSorted.count }
+	@objc			var	objects :[Any] { self.itemIDsSorted.map({ self.itemByItemID[$0]!.object }) }
+
+	@objc			var	compareItemsProc
+							:(_ item1 :Item, _ item2 :Item, _ sortDescriptors :[NSSortDescriptor]) -> Bool =
+							{
+								// Iterate sort descriptors
+								for sortDescriptor in $2 {
+									// Compare
+									switch sortDescriptor.compare($0, to: $1) {
+										case .orderedAscending:		return sortDescriptor.ascending
+										case .orderedDescending:	return !sortDescriptor.ascending
+										case .orderedSame:			break
+									}
+								}
+
+								return false
+							}
+
+			private	var	itemByItemID = [String : Item]()
+
+			private	var	itemIDs = [String]() { didSet { self.itemIDsSorted = self.itemIDs } }
+			private	var	itemIDsSorted = [String]()
+
+			private	var	sortDescriptors = [NSSortDescriptor]()
 
 	// MARK: Instance methods
 	//------------------------------------------------------------------------------------------------------------------
+	public func itemID(at index :Int) -> String { self.itemIDsSorted[index] }
+
+	//------------------------------------------------------------------------------------------------------------------
+	public func index(of itemID :String) -> Int? { self.itemIDsSorted.firstIndex(of: itemID) }
+
+	//------------------------------------------------------------------------------------------------------------------
+	@objc(setItems:)
 	func set(items :[Item]) {
-		// Store
-		self.itemByItemID = Dictionary(items.map({ ($0.id, $0) }))
+		// Reset
+		self.itemByItemID.removeAll()
+		self.itemIDs.removeAll()
+
+		// Add
+		add(items: items)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	@objc(addItems:)
+	func add(items :[Item]) {
+		// Update
+		self.itemByItemID += Dictionary(items.map({ ($0.id, $0) }))
+		self.itemIDs += items.map({ $0.id })
+
+		// Check if have sorting
+		if !self.sortDescriptors.isEmpty {
+			// Update sorting
+			updateSorting()
+		}
 
 		// Note content updated
 		noteContentUpdated()
 	}
 
 	//------------------------------------------------------------------------------------------------------------------
-	func add(items :[Item]) {
+	@objc(removeItems:)
+	func remove(items :[Item]) {
+		// Setup
+		let	itemIDs = items.map({ $0.id })
+
+		// Update
+		self.itemByItemID.removeValues(forKeys: itemIDs)
+		self.itemIDs -= itemIDs
+
+		// Note content updated
+		noteContentUpdated()
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	func object(for itemID :String) -> Any { self.itemByItemID[itemID]!.object }
+
+	//------------------------------------------------------------------------------------------------------------------
+	@objc(objectsFor:)
+	func objects(for itemIDs :[String]) -> [Any] { itemIDs.map({ self.itemByItemID[$0]!.object }) }
+
+	//------------------------------------------------------------------------------------------------------------------
+	@objc(setSortDescriptors:)
+	func set(sortDescriptors :[NSSortDescriptor]) {
 		// Store
-		self.itemByItemID += Dictionary(items.map({ ($0.id, $0) }))
+		self.sortDescriptors = sortDescriptors
+
+		// Check if now have sorting
+		if !self.sortDescriptors.isEmpty {
+			// Update sorting
+			updateSorting()
+		} else {
+			// Restore given order
+			self.itemIDsSorted = self.itemIDs
+		}
 
 		// Note content updated
 		noteContentUpdated()
@@ -53,4 +134,15 @@ public class TableViewBacking : NSObject {
 	// MARK: Subclass methods
 	//------------------------------------------------------------------------------------------------------------------
 	func noteContentUpdated() {}
+
+	// MARK: Private methods
+	//------------------------------------------------------------------------------------------------------------------
+	private func updateSorting() {
+		// Sort
+		self.itemIDsSorted =
+				self.itemIDsSorted
+						.map({ self.itemByItemID[$0]! })
+						.sorted(by: { self.compareItemsProc($0, $1, self.sortDescriptors) })
+						.map({ $0.id })
+	}
 }
